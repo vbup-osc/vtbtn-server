b327bb4f95c6f33a9f95e19c7e4996d744a63c20
imkiva <libkernelpanic@gmail.com>
2020-06-10 21:06:35 +0800

    fix(style): make compiler happy

--- src/com/oruyanke/vtbs/main.kt
+++ src/com/oruyanke/vtbs/main.kt
@@ -10,6 +10,7 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 object ServerConfig {
     var testing = false
+    const val endpoint = ""
 }
 
 @ExperimentalStdlibApi
