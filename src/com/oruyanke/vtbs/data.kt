292edec3356ebadf4301381d627bdee255026f1b
imkiva <libkernelpanic@gmail.com>
2020-06-10 21:23:38 +0800

    feat(data): return localized text as map

--- src/com/oruyanke/vtbs/data.kt
+++ src/com/oruyanke/vtbs/data.kt
@@ -1,24 +1,24 @@
 package com.oruyanke.vtbs
 
-import com.fasterxml.jackson.annotation.JsonSubTypes
-import com.fasterxml.jackson.annotation.JsonTypeInfo
-
 data class Category(val name: String, val groups: List<Group>)
 
 data class Group(val name: String, val voices: List<Voice>)
 
 data class Voice(val url: String, val text: List<LocalizedText>)
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME)
-@JsonSubTypes(
-    JsonSubTypes.Type(LocalizedText.Zh::class, name = "zh"),
-    JsonSubTypes.Type(LocalizedText.En::class, name = "en"),
-    JsonSubTypes.Type(LocalizedText.Jp::class, name = "jp")
-)
-sealed class LocalizedText {
-    abstract val text: String
+data class LocalizedText(val lang: String, val text: String) {
+    companion object {
+        @JvmStatic
+        fun zh(text: String) = LocalizedText("zh", text)
 
-    class Zh(override val text: String) : LocalizedText()
-    class En(override val text: String) : LocalizedText()
-    class Jp(override val text: String) : LocalizedText()
+        @JvmStatic
+        fun en(text: String) = LocalizedText("en", text)
+
+        @JvmStatic
+        fun jp(text: String) = LocalizedText("jp", text)
+    }
 }
+
+fun List<LocalizedText>.toMap(): Map<String, String> =
+    this.map { Pair(it.lang, it.text) }
+        .toMap()
