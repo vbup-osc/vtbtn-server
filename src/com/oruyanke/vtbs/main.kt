e15404718ae232356952fa614cf1f8ad64802f0e
imkiva <libkernelpanic@gmail.com>
2020-06-10 22:28:02 +0800

    feat(app): refactoring

--- src/com/oruyanke/vtbs/main.kt
+++ src/com/oruyanke/vtbs/main.kt
@@ -3,23 +3,38 @@ package com.oruyanke.vtbs
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter
 import io.ktor.application.Application
 import io.ktor.application.install
+import io.ktor.features.CORS
 import io.ktor.features.ContentNegotiation
+import io.ktor.http.HttpMethod
 import io.ktor.jackson.jackson
+import io.ktor.routing.routing
+import org.koin.dsl.module
+import org.koin.ktor.ext.koin
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 object ServerConfig {
-    var testing = false
-    const val endpoint = ""
+    var TESTING = false
+    lateinit var DB_NAME: String
 }
 
 @ExperimentalStdlibApi
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
-    ServerConfig.testing = testing
+    ServerConfig.TESTING = testing
+    ServerConfig.DB_NAME = when (testing) {
+        true -> "vtbtn-debug"
+        false -> "vtbtn-product"
+    }
+
     installFeatures()
-    setupRouter()
+
+    routing {
+        userRoutes()
+    }
 }
 
 fun Application.installFeatures() = run {
@@ -28,4 +43,20 @@ fun Application.installFeatures() = run {
             setDefaultPrettyPrinter(DefaultPrettyPrinter())
         }
     }
+
+    install(CORS) {
+        method(HttpMethod.Options)
+        method(HttpMethod.Put)
+        method(HttpMethod.Delete)
+        method(HttpMethod.Patch)
+        allowCredentials = true
+    }
+
+    koin {
+        modules(module)
+    }
+}
+
+val module = module {
+    single { KMongo.createClient("mongodb://localhost:27017").coroutine }
 }
