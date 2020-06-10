29e6c3ec3bc9bef7445d2c684eac651deca6664d
imkiva <libkernelpanic@gmail.com>
2020-06-11 00:55:45 +0800

    fix(route): revert: #f7c6f3d6

--- src/com/oruyanke/vtbs/bean.kt
+++ src/com/oruyanke/vtbs/bean.kt
@@ -2,7 +2,7 @@ package com.oruyanke.vtbs
 
 data class GroupResponse(val name: String, val desc: Map<String, String>, var voices: List<VoiceResponse>)
 
-data class VoiceResponse(val name: String, val url: String, val desc: Map<String, String>)
+data class VoiceResponse(val name: String, val url: String, val group: String, val desc: Map<String, String>)
 
 data class AddVoiceRequest(val name: String, val url: String, val desc: List<LocalizedText>)
 
