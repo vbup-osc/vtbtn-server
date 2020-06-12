f61529d81c57f14efe6e8a1c95b927b386c131f8
Shirakami Emmmer <yjhdandan@163.com>
2020-06-12 14:43:53 +0800

    ex method

--- src/com/oruyanke/vtbs/mongo-vtuber.kt
+++ src/com/oruyanke/vtbs/mongo-vtuber.kt
@@ -2,13 +2,12 @@ package com.oruyanke.vtbs
 
 import com.mongodb.MongoWriteException
 import org.bson.codecs.pojo.annotations.BsonId
-import org.litote.kmongo.MongoOperator
 import org.litote.kmongo.coroutine.CoroutineClient
 import org.litote.kmongo.coroutine.CoroutineCollection
 import org.litote.kmongo.coroutine.CoroutineDatabase
 import org.litote.kmongo.eq
-import java.text.SimpleDateFormat
-import java.util.*
+import org.litote.kmongo.inc
+import org.litote.kmongo.upsert
 
 data class Group(
     @BsonId val name: String,
@@ -88,8 +87,16 @@ suspend fun CoroutineCollection<Group>.byName(name: String) =
     findOneById(name)
         ?: throw BadRequestException("Group '$name' not found")
 
-//suspend fun CoroutineCollection<Statistic>.plusOne(statistic: Statistic)=
-//   if ()
+suspend fun CoroutineCollection<Statistic>.plusOne(statistic: PlusOneRequest) =
+    updateOne(
+    org.litote.kmongo.and(
+        Statistic::date eq statistic.date,
+        Statistic::group eq statistic.group,
+        Statistic::name eq statistic.name
+    ),
+    inc(Statistic::time, 1),
+    upsert()
+)
 
 
 suspend fun CoroutineCollection<Voice>.addVoice(voice: Voice) =
--- src/com/oruyanke/vtbs/route-statistics.kt
+++ src/com/oruyanke/vtbs/route-statistics.kt
@@ -9,8 +9,6 @@ import io.ktor.routing.route
 import org.koin.ktor.ext.inject
 import org.litote.kmongo.coroutine.CoroutineClient
 import org.litote.kmongo.eq
-import org.litote.kmongo.inc
-import org.litote.kmongo.upsert
 
 
 fun Route.statisticsRoutes() {
@@ -22,53 +20,55 @@ fun Route.statisticsRoutes() {
                 val vtb = param("vtb")
                 var sum = 0
                 mongo.forVtuber(vtb).statistics().find()
-                        .toList()
-                        .forEach {
-                            sum += it.time
-                        }
+                    .toList()
+                    .forEach {
+                        sum += it.time
+                    }
                 call.respond(
-                        mapOf(
-                                "vtuber" to vtb,
-                                "clickTimes" to sum
-                        )
+                    mapOf(
+                        "vtuber" to vtb,
+                        "clickTimes" to sum
+                    )
                 )
             }
         }
+
         get("/{vtb}/{group}") {
             errorAware {
                 val vtb = param("vtb")
                 val group = param("group")
                 var sum = 0
                 mongo.forVtuber(vtb).statistics().find(Statistic::group eq group)
-                        .toList()
-                        .forEach {
-                            sum += it.time
-                        }
+                    .toList()
+                    .forEach {
+                        sum += it.time
+                    }
                 call.respond(
-                        mapOf(
-                                "vtuber" to vtb,
-                                "group" to group,
-                                "clickTimes" to sum
-                        )
+                    mapOf(
+                        "vtuber" to vtb,
+                        "group" to group,
+                        "clickTimes" to sum
+                    )
                 )
             }
         }
+
         get("/{vtb}/{name}") {
             errorAware {
                 val vtb = param("vtb")
                 val voiceName = param("name")
                 var sum = 0
                 mongo.forVtuber(vtb).statistics().find(Statistic::name eq voiceName)
-                        .toList()
-                        .forEach {
-                            sum += it.time
-                        }
+                    .toList()
+                    .forEach {
+                        sum += it.time
+                    }
                 call.respond(
-                        mapOf(
-                                "vtuber" to vtb,
-                                "name" to voiceName,
-                                "clickTimes" to sum
-                        )
+                    mapOf(
+                        "vtuber" to vtb,
+                        "name" to voiceName,
+                        "clickTimes" to sum
+                    )
                 )
             }
         }
@@ -76,15 +76,7 @@ fun Route.statisticsRoutes() {
         post<PlusOneRequest>("/{vtb}") {
             errorAware {
                 val vtb = param("vtb")
-                mongo.forVtuber(vtb).statistics().updateOne(
-                        org.litote.kmongo.and(
-                                Statistic::date eq it.date,
-                                Statistic::group eq it.group,
-                                Statistic::name eq it.name
-                        ),
-                        inc(Statistic::time, 1),
-                        upsert()
-                )
+                mongo.forVtuber(vtb).statistics().plusOne(it)
             }
         }
     }
